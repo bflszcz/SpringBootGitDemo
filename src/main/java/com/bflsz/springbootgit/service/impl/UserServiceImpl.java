e3d164197684bc6b609099fc0946828d1082580e
bflszcz <zcz3030759169@gmail.com>
2025-02-05 19:26:01 +0800

    Update README.md

--- src/main/java/com/bflsz/springbootgit/service/impl/UserServiceImpl.java
+++ src/main/java/com/bflsz/springbootgit/service/impl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class serServiceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserMapper userMapper;
